bebd97bd2394a6c3e27c1e843a58771644a9103c
Sergio Guzman <hiserg98@gmail.com>
2019-06-14 18:57:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+
+def badge_maker(name)
+  
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator (attendees)
+  x = 0
+  attendees.each do |names|
+   attendees.shift("Hello, my name is #{names}.")
+   x += 1
+   
+end 
+return attendees
+end
+
+def assign_rooms
+  
+  
+end
+
+def printer
+  
+  
+end
